39ab63cfbf9eb5b938e9304e9ae47a54c73db13a
Morris Ombiro <morriskalb@gmail.com>
2024-11-15 17:29:12 -0600

    initialize spring boot for rest support

---
+++ backend/src/test/java/com/song_detective/web_app/SongDetectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song_detective.web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongDetectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
